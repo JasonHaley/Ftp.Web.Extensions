54a043d73158fa0ceaa9302fa41bd494e2577720
Jason Haley <jason@jasonhaley.com>
2016-04-03 14:26:57 -0400

    Checked in some missing files

---
+++ source/Ftp.Web.Extensions/Core/Framework/AmbientSettingsProvider.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Ftp.WebJobs.Extensions.Framework
+{
+    public class AmbientSettingsProvider
+    {
+        private static readonly AmbientSettingsProvider Singleton = new AmbientSettingsProvider();
+
+        internal static readonly string Prefix = "AzureWebJobs";
+
+        private AmbientSettingsProvider()
+        {
+        }
+
+        /// <summary>
+        /// Gets the singleton instance.
+        /// </summary>
+        public static AmbientSettingsProvider Instance
+        {
+            get { return Singleton; }
+        }
+
+        /// <summary>
+        /// Attempts to first read a setting from the appSettings configuration section.
+        /// If not found there, it will attempt to read from environment variables.
+        /// </summary>
+        /// <param name="settingName">The name of the setting to look up.</param>
+        /// <returns>The setting, or <see langword="null"/> if no setting was found.</returns>
+        public string GetSetting(string settingName)
+        {
+            // first try prefixing
+            string prefixedSettingName = GetPrefixedSettingName(settingName);
+            string setting = ConfigurationUtility.GetSettingFromConfigOrEnvironment(prefixedSettingName);
+
+            if (string.IsNullOrEmpty(setting))
+            {
+                // next try a direct unprefixed lookup
+                setting = ConfigurationUtility.GetSettingFromConfigOrEnvironment(settingName);
+            }
+
+            return setting;
+        }
+
+        internal static string GetPrefixedSettingName(string settingName)
+        {
+            return Prefix + settingName;
+        }
+    }
+}
---
+++ source/Ftp.Web.Extensions/Core/Framework/ConfigurationUtility.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Configuration;
+
+namespace Ftp.WebJobs.Extensions.Framework
+{
+    // from Microsoft.Azure.WebJobs.Host
+
+    internal static class ConfigurationUtility
+    {
+        public static string GetSettingFromConfigOrEnvironment(string settingName)
+        {
+            string configValue = ConfigurationManager.AppSettings[settingName];
+            if (!string.IsNullOrEmpty(configValue))
+            {
+                // config values take precedence over environment values
+                return configValue;
+            }
+
+            return Environment.GetEnvironmentVariable(settingName) ?? configValue;
+        }
+    }
+}
